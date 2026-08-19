63e43073712734f63107c0c589b4e70c9a6d39f5
Pauline Moraa <pmoraa431@gmail.com>
2026-08-19 17:13:15 +0300

    app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import Hero from "@/components/home/Hero";
-import Services from "@/components/home/Services";  
+import FloralShopPreview from "@/components/home/FloralShopPreview";
+import Services from "@/components/home/Services";
+import FeaturedEvents from "@/components/home/FeaturedEvents";
+import WhyChooseUs from "@/components/home/WhyChooseUs";
+import Testimonials from "@/components/home/Testimonials";
+import ContactCTA from "@/components/home/ContactCTA";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <FloralShopPreview />
       <Services />
-
-      
+      <FeaturedEvents />
+      <WhyChooseUs />
+      <Testimonials />
+      <ContactCTA />
     </main>
   );
 }
